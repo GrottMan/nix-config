dd49a10b89936adfa08d6ec9f1567c2163aeb9fc
Tomas <lundkvist.tomas@gmail.com>
2025-09-04 19:15:06 +0200

    NixOS rebuild: 14 current  2025-09-04 19:15:04  25.05.809261.7c815e513adb  6.12.44                          *

--- configuration.nix
+++ configuration.nix
@@ -159,7 +159,6 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
