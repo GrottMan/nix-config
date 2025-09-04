0659f9f20ecd33616ee1302f69bc89c2f3797675
Tomas <lundkvist.tomas@gmail.com>
2025-09-04 19:25:48 +0200

    NixOS rebuild: 17 current  2025-09-04 19:25:46  25.05.809261.7c815e513adb  6.12.44                          *

--- systempackages.nix
+++ systempackages.nix
@@ -23,4 +23,11 @@
     kdePackages.partitionmanager
     prusa-slicer
   ];
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 }
