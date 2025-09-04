d3aff25fc6e18c31936425835fcc644d8ddd27ee
GrottMan <lundkvist.tomas@gmail.com>
2025-09-04 19:02:22 +0200

    Update systempackages.nix

--- systempackages.nix
+++ systempackages.nix
@@ -1,1 +1,18 @@
+# List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    gitFull
+    (vscode-with-extensions.override {
+      vscodeExtensions = with vscode-extensions; [
+        bbenoist.nix
+      ];
+    })
+    github-desktop
+    gnome-keyring
+    alejandra
+    pkgs.kdePackages.partitionmanager
+    pkgs.prusa-slicer
+  ];
 
