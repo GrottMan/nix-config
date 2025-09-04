429e2c84ad322e22796361649a0ed5a0a1539fba
GrottMan <lundkvist.tomas@gmail.com>
2025-09-04 19:18:05 +0200

    Create alias.nix

---
+++ alias.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+  # ... other configuration options
+
+  # Set aliases globally
+  environment.shellAliases = {
+    ll = "ls -lh";
+    gc = "git commit";
+    update = "./rebuild.sh";
+  };
+}
