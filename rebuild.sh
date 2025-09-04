a5032f078787f908f46c7d3b2f0c569cdec7d10f
GrottMan <lundkvist.tomas@gmail.com>
2025-09-04 18:27:06 +0200

    Add files via upload

---
+++ rebuild.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+cd /etc/nixos/
+git pull
+git add *
+# Autoformat your nix files
+alejandra  /etc/nixos/ &>/dev/null \
+  || ( alejandra /etc/nixos/ ; echo "formatting failed!" && exit 1)
+
+echo "NixOS Rebuilding..."
+
+nh os switch --update
+#nh clean all --keep 3
+
+# Get current generation metadata
+current=$(nixos-rebuild list-generations | grep current)
+
+git commit -am "$current"
+
+echo "Rebuild finished"
+# git push
+# Commit all changes witih the generation metadata
