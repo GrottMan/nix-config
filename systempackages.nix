78d2aa772f75a745d5baa5991d4270cfad274be7
GrottMan <lundkvist.tomas@gmail.com>
2025-09-05 13:26:32 +0200

    Update systempackages.nix

--- systempackages.nix
+++ systempackages.nix
@@ -1,3 +1,4 @@
+# Start of systempackages.nix
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
@@ -54,6 +55,7 @@
     enable = true;
     package = pkgs.lact;
   };
+# End of systempackages.nix
 
   # NFS support in the initial ramdisk.
   boot.initrd.supportedFilesystems = [ "nfs" ];
