05202cb1b1cf32685f2e9ba8b2880379b2ef8725
GrottMan <lundkvist.tomas@gmail.com>
2025-09-05 13:25:27 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,3 +1,4 @@
+# Start of configuration.nix
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
@@ -142,6 +143,7 @@
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
+  # End of configuration.nix
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
