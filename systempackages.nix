589a156fefa86d41eebc4b65d09940ba237cd38d
GrottMan <lundkvist.tomas@gmail.com>
2025-09-05 13:30:23 +0200

    Update systempackages.nix

--- systempackages.nix
+++ systempackages.nix
@@ -1,15 +1,9 @@
 # Start of systempackages.nix
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-# sudo nixos-rebuild switch
-# List packages installed in system profile. To search, run:
-# $ nix search wget
+# This file defines the system-wide packages to be installed.
 
 {pkgs, ...}: {
   # This block defines the system-wide packages.
   environment.systemPackages = with pkgs; [
-    firefox
     neofetch
     htop
     btop-rocm
@@ -55,9 +49,9 @@
     enable = true;
     package = pkgs.lact;
   };
-# End of systempackages.nix
 
   # NFS support in the initial ramdisk.
   boot.initrd.supportedFilesystems = [ "nfs" ];
   boot.initrd.kernelModules = [ "nfs" ];
 }
+# End of systempackages.nix
